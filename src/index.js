ab049a9bd7c3c772fc431fb12505ff240f2204b3
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-06-22 16:14:53 +0300

    declares the variables in jS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const MenuBar = document.getElementById("Menu-bar")
+const form =document.getElementById("comment-form")
+const image = document.getElementsByTagName("img")
+const  text = document.getElementById("comment")
 const linkUrl = "http://localhost:3000/categories"
-
+const submit =document.getElementById("submit")
 fetch(linkUrl)
     .then(response => response.json())
-    .then(jsonData => {})
+    .then(jsonData => {}
+    )
         
 
