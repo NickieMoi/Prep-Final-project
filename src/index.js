e6a41ff6c904f8403cfbd5c69108579dd4abe263
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-06-22 15:31:52 +0300

    introduces the api link

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const MenuBar = document.getElementById("Menu-bar")
+const linkUrl = "http://localhost:3000/categories"
+
+fetch(linkUrl)
+    .then(response => response.json())
+    .then(jsonData => {})
+        
+
