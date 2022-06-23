ef54dcb9df34d2978611ec0478812236dd9968e9
Nicholas <nicholas.kipkirui@student.moringaschool.com>
2022-06-23 22:48:54 +0300

    add js files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,56 @@
-const MenuBar = document.getElementById("Menu-bar")
-const form =document.getElementById("comment-form")
-const image = document.getElementsByTagName("img")
-const  text = document.getElementById("comment")
+// const MenuBar = document.getElementById("Menu-bar")
+// const form =document.getElementById("comment-form")
+// const image = document.getElementsById("img")
+// const  text = document.getElementById("comment")
+// const linkUrl = "http://localhost:3000/categories"
+// const submit =document.getElementById("submit")
+// const meal =document.getElementById("name")
+// const menuPro=document.querySelector(".menu-procedure")
+// const Description =document.getElementById("detailed-info")
 const linkUrl = "http://localhost:3000/categories"
-const submit =document.getElementById("submit")
+const meal =document.getElementById("name")
+const image = document.getElementById("img")
+const Description =document.getElementById("detailed-info")
+const menu2 =document.getElementById("menu1")
+
+function foodDisplay(foodDescription){
+    meal.innerText =foodDescription.strCategory
+    image.src =foodDescription.strCategoryThumb
+    Description.innerText =foodDescription.strCategoryDescription
+
+
+     
+ }
+
+
 fetch(linkUrl)
     .then(response => response.json())
-    .then(jsonData => {}
-    )
+    .then(jsonData => {
         
+foodDisplay(jsonData[1])
+menuDisplay(jsonData)
+    })
+     function menuDisplay(delicacies){
+ delicacies.forEach(foodDescription=>{
+    const li =document.createElement("li") 
+    li.className ="delicacie"
+    li.innerText =foodDescription.strCategory
+menu2.append(li)
+li.addEventListener("click",()=>{
+    foodDisplay(foodDescription)
+})
+ })
 
+     }
+
+
+        
+     document.getElementById("comment-form").addEventListener("click", function(event){
+        event.preventDefault()
+    
+     function addComment(){
+        let Name = document.getElementById('addcomment1').value; 
+        document.getElementById('addcomment').innerHTML += Name;
+    }    
+    addComment()
+});
